b7ee978d2bf2b0d8c2d18936c04beaf2a3a08a20
JoeyJanssen11 <josephjanssen11@gmail.com>
2022-05-05 19:13:55 -0700

    Hey

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state=({apiResponse:""});
+  }
+
+  callAPI(){
+    fetch("http://localhost:9000/testAPI")
+    .then(res => res.text())
+    .then(res => this.setState({apiResponse: res}));
+  }
+
+  componentWillMount(){
+    this.callAPI();
+  }
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{this.state.apiResponse}</p>
     </div>
   );
+}
 }
 
 export default App;
